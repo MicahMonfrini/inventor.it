b07546c4b5e828658b35b701ccaf77035ade36f9
MicahMonfrini <ffrock905@gmail.com>
2017-05-13 12:11:21 -0500

    init repo

---
+++ src/actions/index.js
@@ -0,0 +1,24 @@
+export function increaseCounter(){
+  return {
+    type:"INCREASE_COUNTER"
+  }
+}
+
+export function setSpecialText(txt){
+  return {
+    type:"SET_SPECIAL_TEXT",
+    value:txt
+  }
+}
+
+export function addUser(user){
+  return {
+    type:"ADD_USER",
+    value:user
+  }
+}
+export function removeUser(){
+  return {
+    type:"REMOVE_USER"
+  }
+}
