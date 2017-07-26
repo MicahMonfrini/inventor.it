00b1e4108b40c83eb82b022abea1fdc4df17faed
MicahMonfrini <ffrock905@gmail.com>
2017-07-25 20:14:21 -0500

    messing around with react-bootstrap-table

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import Link from "next/link";
 import PropTypes from "prop-types";
+import {BootstrapTable, TableHeaderColumn} from "react-bootstrap-table";
 
 class Books extends Component {
   constructor(props) {
@@ -8,32 +9,35 @@ class Books extends Component {
   }
 
   render() {
-    // map over books array. will pass to component via props
-    const bookList = this.props.books.map((book, index) => {
-      return (
-        <ul key={index}>
-          <li><strong>Title:</strong> {book.title}</li>
-          <li><strong>Author:</strong> {book.author}</li>
-          <li><strong>Category:</strong> {book.category}</li>
-          {/* @TODO: configure url to generate unique title for detail page */}
-          <Link href={{
-            pathname: "bookdetail",
-            query: {
-              id: book._id
-            }
-          }}>
-            <a>More info</a>
-          </Link>
-          <br />
-          <button onClick={() => {
-            this.props.deleteBook(book._id);
-          }}>Delete</button>
-        </ul>
-      );
-    });
+    // // map over books array. will pass to component via props
+    // const bookList = this.props.books.map((book, index) => {
+    //   return (
+    //     <ul key={index}>
+    //       <li><strong>Title:</strong> {book.title}</li>
+    //       <li><strong>Author:</strong> {book.author}</li>
+    //       <li><strong>Category:</strong> {book.category}</li>
+    //       {/* @TODO: configure url to generate unique title for detail page */}
+    //       <Link href={{
+    //         pathname: "bookdetail",
+    //         query: {
+    //           id: book._id
+    //         }
+    //       }}>
+    //         <a>More info</a>
+    //       </Link>
+    //       <br />
+    //       <button onClick={() => {
+    //         this.props.deleteBook(book._id);
+    //       }}>Delete</button>
+    //     </ul>
+    //   );
+    // });
     return (
       <div>
-        {bookList}
+        <BootstrapTable data={this.props.books}>
+          <TableHeaderColumn dataField="title" isKey>Title</TableHeaderColumn>
+          <TableHeaderColumn dataField="author">Author</TableHeaderColumn>
+        </BootstrapTable>
         <Link href="/submit">
           <a>Add New Book</a>
         </Link>
